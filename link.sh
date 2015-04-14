e14d5140301cb6d90465adfa00a1bf459c13075f
Raf Kaplon <raf.kaplon@youi.tv>
2015-04-14 16:14:24 -0400

    Update link.sh to echo skipped links

--- link.sh
+++ link.sh
@@ -5,10 +5,8 @@ function link {
     source=$1
     target="${HOME}/.$source"
 
-    if [ -f "$target" ]; then
-        return
-    elif [ -d "$target" ]; then
-        return
+    if [ -e "$target" ]; then
+        echo "Skipping $source -> $target"
     else
         echo "Linking $source -> $target"
         ln -s "`pwd`/$source" "$target"
